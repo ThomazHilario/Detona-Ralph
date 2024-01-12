93062091fa1945e1908c1efb145a6ebfd27969c5
Thomaz Alves <thomazhilario5@gmail.com>
2024-01-12 15:46:06 -0300

    Logando o usuario com o firebase

--- src/Pages/Login/index.jsx
+++ src/Pages/Login/index.jsx
@@ -1,8 +1,13 @@
-import { useState } from 'react'
-import { Link } from 'react-router-dom'
 import './login.css'
+import { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import {database, auth} from '../../Firebase'
+import { signInWithEmailAndPassword } from 'firebase/auth'
 
 export const Login = () => {
+
+    // navigate
+    const navigate = useNavigate()
 
     // state - email
     const [email, setEmail] = useState('')
@@ -10,22 +15,43 @@ export const Login = () => {
     // state - password
     const [password, setPassword] = useState('')
 
+    // singInUser - logando usuario
+    async function singInUser(e){
+        try {
+
+            // Cancelando envio do formulario
+            e.preventDefault()
+
+            // Logando usuario
+            const user = await signInWithEmailAndPassword(auth, email, password)      
+
+            // Salvando dados na localStorage
+            localStorage.setItem('@user',JSON.stringify(user.user))
+
+            // Navegando ate a rota do jogo
+            navigate('/game')
+
+        } catch (e) {
+            console.log(e)
+        }
+    }
+
     return(
         <main className="mainLogin">
             <form>
 
                 <div>
                     <label>Email:</label>
-                    <input type="text" value={email}/>
+                    <input type="text" value={email} onChange={(e) => setEmail(e.target.value)}/>
                 </div>
 
                 <div>
                     <label>Password:</label>
-                    <input type="text" value={password}/>
+                    <input type="password" value={password} onChange={(e) => setPassword(e.target.value)}/>
                 </div>
 
                 <div>
-                    <button>Entrar</button>
+                    <button onClick={singInUser}>Entrar</button>
                     <Link to='/' className='navigateHome'>Não possui uma conta ? <strong>Cadastre-se</strong></Link>
                 </div>
 
