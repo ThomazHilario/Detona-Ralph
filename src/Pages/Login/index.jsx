a0ee3d0423c871bdb82b0009fe657e36e8120bd5
Thomaz Alves <thomazhilario5@gmail.com>
2024-01-12 15:30:48 -0300

    Criando o componente Login

--- src/Pages/Login/index.jsx
+++ src/Pages/Login/index.jsx
@@ -1,5 +1,35 @@
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
+import './login.css'
+
 export const Login = () => {
+
+    // state - email
+    const [email, setEmail] = useState('')
+
+    // state - password
+    const [password, setPassword] = useState('')
+
     return(
-        <h1>hello world</h1>
+        <main className="mainLogin">
+            <form>
+
+                <div>
+                    <label>Email:</label>
+                    <input type="text" value={email}/>
+                </div>
+
+                <div>
+                    <label>Password:</label>
+                    <input type="text" value={password}/>
+                </div>
+
+                <div>
+                    <button>Entrar</button>
+                    <Link to='/' className='navigateHome'>Não possui uma conta ? <strong>Cadastre-se</strong></Link>
+                </div>
+
+            </form>
+        </main>
     )
 }
