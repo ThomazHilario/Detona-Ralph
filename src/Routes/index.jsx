fdf12d18233162acaeb5ccd7c0fddfe392c9fae8
Thomaz Alves <thomazhilario5@gmail.com>
2024-01-13 16:22:20 -0300

    retirando dois componentes

--- src/Routes/index.jsx
+++ src/Routes/index.jsx
@@ -11,8 +11,7 @@ export const RoutesPage = () => {
             <Routes>
                 <Route path='/' element={ <Home/> } />
                 <Route path='/login' element = { <Login/> }/>
-                <Route path='/game' element={ <MainGame/> }/>
-                <Route path='/ranking' element={ <Ranking/> }/>
+
             </Routes>
         </BrowserRouter>
     )
